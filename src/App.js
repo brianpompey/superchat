a2cf1c929e52175b8092d6651a11d54e0e23c1bf
brianpompey <brianthescholar@gmail.com>
2021-09-08 16:55:43 -0400

    Added firebase config

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 
+firebase.initializeApp({
+  apiKey: "AIzaSyD1gV1Ieb_85-TjoulqE4xndjQVmpyRLgE",
+  authDomain: "superchat-9c46b.firebaseapp.com",
+  projectId: "superchat-9c46b",
+  storageBucket: "superchat-9c46b.appspot.com",
+  messagingSenderId: "765330108152",
+  appId: "1:765330108152:web:b264a080efd8b02e17950d",
+  measurementId: "G-QS8NP9PF04"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
